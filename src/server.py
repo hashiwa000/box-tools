a28bb583b3644d028a9d42298754c00df474835a
hashiwa000 <gate000jp@gmail.com>
2021-02-26 14:10:36 +0000

    add server.py

---
+++ src/server.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import http.server
+
+
+class HttpHandler(http.server.BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers.get("content-length"))
+        body = self.rfile.read(content_length).decode("utf-8")
+        print('body = {}'.format(body))
+        body = 'Hello, world!'
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.send_header('Content-length', len(body.encode()))
+        self.end_headers()
+        self.wfile.write(body.encode())
+
+
+if __name__ == '__main__':
+    host = ''
+    port = 80
+    httpd = http.server.HTTPServer((host, port), HttpHandler)
+    print('server started: %s' % port)
+    httpd.serve_forever()
