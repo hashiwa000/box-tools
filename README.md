afebc5d016ef95804e6871e8d562a3252a7a6194
hashiwa000 <gate000jp@gmail.com>
2021-02-26 14:37:51 +0000

    update

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # box-tools
 
+## Environment
+
 - Python 3.6.8
 - CentOS Linux release 7.6.1810 (Core)
+
+## How to use
 
 ```sh
 npx localtunnel --port 80
@@ -14,3 +18,6 @@ python3 server.py
 ```sh
 curl -X POST "https://xx.yy.zz" -v -d 'hoge'
 ```
+
+https://app.box.com/developers/console
+
--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import http.server
-
+import json
 
 class HttpHandler(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
@@ -9,6 +9,12 @@ class HttpHandler(http.server.BaseHTTPRequestHandler):
         print('content_length = {}'.format(content_length))
         body = self.rfile.read(content_length).decode("utf-8")
         print('body = {}'.format(body))
+        try:
+            body = json.loads(body)
+            access_token = body.get('token', {}).get('read', {}).get('access_token')
+            print('access_token = {}'.format(access_token))
+        except json.decoder.JSONDecodeError as e:
+            pass
         body = 'Hello, world!'
         self.send_response(200)
         self.send_header('Content-type', 'text/html; charset=utf-8')
